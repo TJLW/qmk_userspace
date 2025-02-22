b9f1a4a582d392e8e2df2520299392d21c566421
Taylor JL Whitaker <taylor.whitaker@astrobotic.com>
2025-02-22 02:00:13 -0500

    Try a simpler LGUI vs LCTL implementation

--- keyboards/sofle_choc/keymaps/tjlw/keymap.c
+++ keyboards/sofle_choc/keymaps/tjlw/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC, 
     KC_NO,      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                               KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT, 
     KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    KC_MPLY,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT, 
-                            KC_LCTL,    KC_LSFT,    MO(4),      MO(3),      KC_SPC,     KC_SPC,     MO(3),      KC_EXEC,    KC_NO,      TO(0)
+                            KC_LCTL,    KC_LSFT,    KC_LCTL,    MO(3),      KC_SPC,     KC_SPC,     MO(3),      KC_EXEC,    KC_NO,      TO(0)
 ),
 
 // Mac layer
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC, 
     KC_NO,      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                               KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT, 
     KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    KC_MPLY,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT, 
-                            KC_LCTL,    KC_LSFT,    MO(5),      MO(3),      KC_SPC,     KC_SPC,     MO(3),      KC_EXEC,    KC_NO,      TO(0)
+                            KC_LCTL,    KC_LSFT,    KC_LGUI,    MO(3),      KC_SPC,     KC_SPC,     MO(3),      KC_EXEC,    KC_NO,      TO(0)
 ),
 
 // Common modifers
