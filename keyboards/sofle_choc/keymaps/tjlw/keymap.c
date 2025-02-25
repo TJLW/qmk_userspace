257e5df9abda06d4eac3a29dcbd10e60eb5d6830
Taylor JL Whitaker <taylor.whitaker@astrobotic.com>
2025-02-24 21:52:09 -0500

    Put shift and alt where they should be.

--- keyboards/sofle_choc/keymaps/tjlw/keymap.c
+++ keyboards/sofle_choc/keymaps/tjlw/keymap.c
@@ -69,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS, 
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC, 
     KC_NO,      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                               KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT, 
-    KC_NO,      KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    KC_MPLY,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT, 
-                            KC_LCTL,    KC_LSFT,    KC_LCTL,    MO(3),      KC_SPC,     KC_SPC,     MO(3),      KC_EXEC,    KC_NO,      TO(0)
+    KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    KC_MPLY,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT, 
+                            KC_LCTL,    KC_LALT,    KC_LCTL,    MO(3),      KC_SPC,     KC_SPC,     MO(3),      KC_EXEC,    KC_NO,      TO(0)
 ),
 
 // Mac layer
@@ -78,16 +78,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS, 
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC, 
     KC_NO,      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                               KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT, 
-    KC_NO,      KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    KC_MPLY,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT, 
-                            KC_LCTL,    KC_LSFT,    KC_LGUI,    MO(3),      KC_SPC,     KC_SPC,     MO(3),      KC_EXEC,    KC_NO,      TO(0)
+    KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    KC_MPLY,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT, 
+                            KC_LCTL,    KC_LALT,    KC_LGUI,    MO(3),      KC_SPC,     KC_SPC,     MO(3),      KC_EXEC,    KC_NO,      TO(0)
 ),
 
 // Common modifers
+// [3] = LAYOUT(
+//     KC_GRV,     KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,                            KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_UNDS, 
+//     KC_TILD,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                              KC_NO,      KC_NO,      KC_UP,      KC_LBRC,    KC_RBRC,    KC_DEL, 
+//     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_FIND,    KC_NO,                              KC_NO,      KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_COLN,    KC_DQUO, 
+//     KC_TRNS,    KC_UNDO,    KC_CUT,     KC_COPY,    KC_PSTE,    KC_NO,      KC_NO,      KC_NO,      KC_EQL,     KC_PLUS,    KC_LT,      KC_GT,      KC_QUES,    KC_PIPE,  
+//                             KC_NO,      KC_LALT,    KC_NO,      KC_TRNS,    KC_NO,      KC_NO,      KC_TRNS,    KC_NO,      KC_NO,      KC_NO
+// ),
 [3] = LAYOUT(
     KC_GRV,     KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,                            KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_UNDS, 
     KC_TILD,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                              KC_NO,      KC_NO,      KC_UP,      KC_LBRC,    KC_RBRC,    KC_DEL, 
     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_FIND,    KC_NO,                              KC_NO,      KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_COLN,    KC_DQUO, 
-    KC_TRNS,    KC_UNDO,    KC_CUT,     KC_COPY,    KC_PSTE,    KC_NO,      KC_NO,      KC_NO,      KC_EQL,     KC_PLUS,    KC_LT,      KC_GT,      KC_QUES,    KC_PIPE,  
+    KC_NO,      KC_UNDO,    KC_CUT,     KC_COPY,    KC_PSTE,    KC_NO,      KC_NO,      KC_NO,      KC_EQL,     KC_PLUS,    KC_LT,      KC_GT,      KC_QUES,    KC_PIPE,  
                             KC_NO,      KC_LALT,    KC_NO,      KC_TRNS,    KC_NO,      KC_NO,      KC_TRNS,    KC_NO,      KC_NO,      KC_NO
 ),
 
